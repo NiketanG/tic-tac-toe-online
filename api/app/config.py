cc06a4750a5a7af99034932e0a20de54efe06f27
NiketanG <nikegulekar@gmail.com>
2020-04-28 21:50:52 +0530

    Fixed Database Path

--- api/app/config.py
+++ api/app/config.py
@@ -2,7 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = '57bde4dd-0da1-41dd-b5bc-38d5997d0fc1'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:N!kketanGT16@localhost/tictactoe'
-    #os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or 'postgresql://postgres:N!kketanGT16@localhost/tictactoe'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
